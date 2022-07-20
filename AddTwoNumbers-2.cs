bcebd2f4afe6462851bb24265c3f2c90a8b66dd2
dolomiten213 <dolomiten213@gmail.com>
2022-07-20 15:16:25 +0300

    2 Task solved

--- AddTwoNumbers-2.cs
+++ AddTwoNumbers-2.cs
@@ -8,66 +8,45 @@ namespace LeetCode
 {
     public partial class Solution
     {
-         public class ListNode {
-             public int val;
-             public ListNode next;
-             public ListNode(int val=0, ListNode next=null) {
-                 this.val = val;
-                 this.next = next;
-    }
-}
-
-        //public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
-        //{
-        //    ListNode Sum(ListNode? l1, ListNode? l2, bool plusOne)
-        //    {
-        //        var newValue = l1?.val ?? 0 + l2?.val ?? 0;
-        //        var addOne = false;
-        //        if (plusOne) newValue++;
-        //        if (newValue > 9)
-        //        {
-        //            newValue %= 10;
-        //            addOne = true;
-        //        }
-        //        if (l1?.next is null && l2?.next is null)
-        //        {
-        //            if (addOne)
-        //            {
-        //                var final = new ListNode(1, null);
-        //                return new ListNode(newValue, final);
-        //            }
-        //            else
-        //            {
-        //                return new ListNode(newValue, null);
-        //            }
-        //        }
-        //        return new ListNode(newValue, Sum(l1?.next, l2?.next, addOne));
-        //    }
-
-        //    return Sum(l1, l2, false);
-        //}
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
 
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            long ParseToInt(ListNode node)
+            ListNode Sum(ListNode? l1, ListNode? l2, bool plusOne)
             {
-                long mp = 1;
-                long res = 0;
-                while (true)
+                var newValue = l1?.val ?? 0 + l2?.val ?? 0;
+                var addOne = false;
+                if (plusOne) newValue++;
+                if (newValue > 9)
                 {
-                    res += node.val * mp;
-                    mp *= 10;
-                    if (node.next == null) return res;
-                    node = node.next;
+                    newValue %= 10;
+                    addOne = true;
                 }
-            }
-            ListNode IntToNode(long num)
-            {
-                if (num < 10) return new ListNode((int)num, null);
-                return new ListNode(((int)num) % 10, IntToNode(num / 10));
+                if (l1?.next is null && l2?.next is null)
+                {
+                    if (addOne)
+                    {
+                        var final = new ListNode(1, null);
+                        return new ListNode(newValue, final);
+                    }
+                    else
+                    {
+                        return new ListNode(newValue, null);
+                    }
+                }
+                return new ListNode(newValue, Sum(l1?.next, l2?.next, addOne));
             }
 
-            return IntToNode(ParseToInt(l1) + ParseToInt(l2));
+            return Sum(l1, l2, false);
         }
     }
 }
