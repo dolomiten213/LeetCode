1cfb0561aa0128a6d6d6e54e1c0f21886a3da134
dolomiten213 <dolomiten213@gmail.com>
2022-07-20 15:13:09 +0300

    1 Task solved

---
+++ TwoSum-1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i])) return new int[2] { i, dict[nums[i]] };
+                if (!dict.ContainsKey(target - nums[i])) dict.Add(target - nums[i], i);
+            }
+            return null;
+        }
+    }
+}
